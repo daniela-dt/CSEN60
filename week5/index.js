80897931299b96a38d635451ba12335c15510441
Daniela Diaz <danieladiaz9091@gmail.com>
2024-10-21 11:32:03 -0700

    added week5 + wrote some js

---
+++ week5/index.js
@@ -0,0 +1,20 @@
+// console.log("hello world")
+//alert('Hello, world!');
+window.document.write('<p>Welcome to CSEN60!</p>');
+
+//document.append('<p>This should come after the h1</p>')
+
+/*
+  Function to calculate the sum of two numbers.
+  Inputs: a, b - numbers to be added.
+  Output: returns the sum of a and b.
+*/
+
+//TODO fix this 
+/** 
+ * 
+ * @description this adds two numbers
+ */
+function add(a, b) {
+    return a + b;
+}
