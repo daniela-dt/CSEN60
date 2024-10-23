2d3ed604b10739ce4fc5f40939eeb3fadde6c0c9
Daniela Diaz <danieladiaz9091@gmail.com>
2024-10-23 11:34:51 -0700

    learned abotu variables and operators

--- week5/index.js
+++ week5/index.js
@@ -1,6 +1,6 @@
 // console.log("hello world")
 //alert('Hello, world!');
-window.document.write('<p>Welcome to CSEN60!</p>');
+//window.document.write('<p>Welcome to CSEN60!</p>');
 
 //document.append('<p>This should come after the h1</p>')
 
@@ -18,3 +18,43 @@ window.document.write('<p>Welcome to CSEN60!</p>');
 function add(a, b) {
     return a + b;
 }
+
+let x = 100;
+//let x -> declaration
+//x = 200 -> initialization
+if (true) {
+    var x = 200; // Same variable!
+}
+console.log(x);//outputs 200
+
+let y 
+y = null
+let z = 0
+console.log(y,z)
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date().getHours()}`;
+
+conscole.log(response) 
+
+const age = 20
+
+if(age>18){
+    console.log("You are of age")
+} else{
+    console.log(`You aren't of age. Please wait ${18-age} year(s)`)
+}
+let isAvailable = true;
+let isOVerAge = (age>18);
+
+console.log("/n", isOverAge)
+
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person["age"])
+
+console.log(typeof("hello world"))
